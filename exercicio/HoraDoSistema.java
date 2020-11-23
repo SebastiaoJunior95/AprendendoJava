9eeb3eb3fd2e7ca20ed2593d1a1cfb67bb3bee5a
SebastiaoJunior95 <sebastiaor95@gmail.com>
2020-11-22 22:27:00 -0300

    ex001 saber a hora do sistema

---
+++ exercicio/HoraDoSistema.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package horadosistema;
+
+import java.util.Date;
+
+/**
+ *
+ * @author cyjuwork
+ */
+public class HoraDoSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");  
+        System.out.println(relogio.toString());
+               
+        // TODO code application logic here
+    }
+    
+}
