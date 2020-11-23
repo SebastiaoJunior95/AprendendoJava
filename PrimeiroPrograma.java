9b1049a60078cb87da0799fe73b3cac2cd58f0fc
SebastiaoJunior95 <sebastiaor95@gmail.com>
2020-11-22 22:17:25 -0300

    Primeiro Programa java Ola mundo

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeiroprograma;
+
+/**
+ *
+ * @author cyjuwork
+ */
+public class PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Olá! munod");
+        
+        // TODO code application logic here
+    }
+    
+}
